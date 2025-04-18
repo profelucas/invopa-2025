e4af518618fcca5694ef7534b6e54aab6376ec2a
profelucas <lucas.almonacid@ulagos.cl>
2025-04-18 18:21:32 -0400

    ejemplo de c&b

--- ra1/ejemplo2.py
+++ ra1/ejemplo2.py
@@ -4,9 +4,9 @@ import pulp as pl
 ejemplo2=pl.LpProblem("tutorial",pl.LpMaximize)
 
 #variables
-x1= pl.LpVariable('x1', lowBound=0, upBound=1, cat='Integer')
-x2= pl.LpVariable('x2', lowBound=0, upBound=1, cat='Integer')
-x3=pl.LpVariable('x3', lowBound=0, upBound=1, cat='Integer')
+x1= pl.LpVariable('x1', lowBound=0, upBound=1, cat='Continuous')
+x2= pl.LpVariable('x2', lowBound=0, upBound=1, cat='Continuous')
+x3=pl.LpVariable('x3', lowBound=0, upBound=1, cat='Continuous')
 
 #funcion objetivo
 
@@ -17,6 +17,28 @@ ejemplo2+=4*x1+3*x2+2*x3,"z"
 ejemplo2+=6*x1+4*x2+3*x3<=8.5,"r1"
 ejemplo2+=2*x1+x2+2*x3<=3,"r2"
 ejemplo2+=4*x1+2*x2+3*x3<=7,"r3"
+
+
+#x1->0,75
+#x2->1
+#x3->0
+#z->6
+
+#cob
+#rama 1 x1<=0
+#ejemplo2+=x1<=0,"cb1" #x1->0, x2->1, x3->1, z->5  cota
+##fin
+
+#rama 2 x1>=1
+ejemplo2+=x1>=1,"co2"#x1->1, x2->0,625, x3->0 z->5,875
+
+#rama 2.1 x1>=1 y x2<=0
+
+ejemplo2+=x2<=0,"co2.1"#x1->1, x2->0, x3->0,5 z->5
+
+#rama 2.2 x1>=1 y x2<=0
+
+ejemplo2+=x2>=1,"co2.2"#inf
 
 
 #solver
